158049b8c41a138c76389b9123189a5ac1d23082
George499 <gi.32@mail.ru>
2019-10-15 13:21:38 +0300

    7

--- webpack.config.js
+++ webpack.config.js
@@ -91,7 +91,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, "./dist"),
       filename: "[name].[hash].build.js",
-      publicPath: isProductionBuild ? publicPath : "",
+      publicPath: isProductionBuild ? publicPath : "{Autumn}/dist",
       chunkFilename: "[chunkhash].js"
     },
     module: {
