9c25fce8e417bf0a9e300351068cef121946e226
George499 <gi.32@mail.ru>
2019-10-21 21:34:33 +0300

    5

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,2 +1,30 @@
+import Vue from 'vue';
+import Swiper from 'swiper'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
+
+
+new Vue({
+    el: "#reviews__slider",    
+    name: 'slider',   
+    components: {
+        swiper,
+        swiperSlide
+      },
+    data() {
+        return {
+            reviews: [],
+            swiperOptions: {
+                navigation : {
+                    nextEl: '.reviews__scroll--left',
+                    prevEl: '.reviews__scroll--right',
+                },
+                loop: true,
+                slidesPerView: 2,
+                slidesPerGroup: 2,         
+                
+              }
+        };
+    },
+})
 
