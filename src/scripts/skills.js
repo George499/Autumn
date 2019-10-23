d12a721ec29829a2b47cd908354d161cbd1382d4
George499 <gi.32@mail.ru>
2019-10-23 17:53:20 +0300

    skills

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,7 +1,54 @@
 import Vue from 'vue';
-import Swiper from 'swiper'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
-import Parallax from '../../node_modules/vue-parallaxy/dist/vue-parallaxy.js'
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+
+const skill = {
+    template: "#skill",
+    props: {
+      skillName: String,
+      skillPercent: Number
+    },
+    methods: {
+      drawColoredCircle() {
+        const circle = this.$refs["color-circle"];
+        const dashArray = parseInt(
+          getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+        );
+        const percent = (dashArray / 100) * (100 - this.skillPercent);
+  
+        circle.style.strokeDashoffset = percent;
+      }
+    },
+    mounted() {
+      this.drawColoredCircle();
+    }
+  };
+  
+  const skillsRow = {
+    template: "#skills-row",
+    components: {
+      skill
+    },
+    props: {
+      skill: Object
+    }
+  };
+  
+  new Vue({
+    el: "#skills-component",
+    template: "#skills-list",
+    components: {
+      skillsRow
+    },
+    data() {
+      return {
+        skills: {}
+      };
+    },
+    created() {
+      const data = require("../data/skills.json");
+      this.skills = data;
+    }
+  });
 
 
 new Vue({
@@ -10,23 +57,19 @@ new Vue({
     components: {
         swiper,
         swiperSlide
-      },
+    },
     data() {
         return {
             reviews: [],
             swiperOptions: {
-                navigation : {
+                navigation: {
                     nextEl: '.reviews__scroll--left',
                     prevEl: '.reviews__scroll--right',
-                },
-                loop: true,
-                slidesPerView: 2,
-                slidesPerGroup: 2,
-                spaceBetween: 84,
-                
-              }
+                  },
+                  loop: true,
+                  slidesPerView: 2,
+                  slidesPerGroup: 2
+            }
         };
-    },
+    },    
 })
-
-
