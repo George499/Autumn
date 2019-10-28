084b57a40a7e3bbe641be410f8417767213226ff
George499 <gi.32@mail.ru>
2019-10-29 02:52:48 +0300

    jj

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,6 @@
-<template lang="pug">
-  h1 Welcome to the Vue App
+<template lang="pug" url="index.pug">
+<styles lang="postcss" url="admin.pcss">
+  .div
+    .header
+      .container.admin-container Админка
 </template>
