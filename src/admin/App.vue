3c1fac94a6fca8f00d83afc7cfea4afc8aa2eead
George499 <gi.32@mail.ru>
2019-10-31 03:43:21 +0300

    7

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,59 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  div
+    adminheader
+    navigation
+    aboutme
+    works
+    worksadd
+    comments
+    commentsadd
 </template>
+
+<script>
+
+
+import adminheader from './components/adminheader.vue';
+import navigation from './components/navigation.vue';
+import aboutme from './components/aboutme.vue';
+import works from './components/works.vue';
+import worksadd from './components/worksadd.vue';
+import comments from './components/comments.vue';
+import commentsadd from './components/commentsadd.vue';
+
+
+export default {
+  name: 'app',
+  components: {
+    adminheader,
+    navigation,
+    aboutme,
+    works,
+    worksadd,
+    comments,
+    commentsadd
+  },
+  data() {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style lang="pcss">
+
+html, body {
+  height: 100%; 
+}
+body {
+  font-family: 'Open Sans', Helvetica, sans-serif;
+  font-size: 16px;
+  line-height: 1.42;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgba(#fff, 0.8);
+  margin: 0
+}
+
+
+</style>
--- src/admin/components/adminheader.vue
+++ src/admin/components/adminheader.vue
@@ -1,3 +1,36 @@
 <template lang="pug">
-    header.header Прривет
+    header.adminheader
+        .admin-container            
+            h1  Георгий Исаев
+            h2 Панель администрирования
+
 </template>
+
+<style lang='pcss' scoped>
+h1 {
+  margin: 0;
+  font-size: 18px;
+  margin-right: 28px;
+}
+h2 {
+    margin: 0;
+    font-size: 14px;
+    color: rgba(#fff, .5)
+}
+.adminheader {
+    width: 100%;
+    height: 80px;
+    background: linear-gradient(90deg, #3e3e59 0%, #454573 100%);
+}
+.admin-container {
+    display: flex;
+    margin: 0 auto;
+    max-width: 1480px;
+    width: 95%;
+    position: relative;  
+    align-items: center;
+    height: 100%;
+
+}
+
+</style>
