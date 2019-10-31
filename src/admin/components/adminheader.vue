a0309708afa8f960873de139cb36714eeeebbf0d
George499 <gi.32@mail.ru>
2019-10-31 16:55:04 +0300

    9

--- src/admin/components/adminheader.vue
+++ src/admin/components/adminheader.vue
@@ -1,36 +1,14 @@
+include './mixins.pug'
 <template lang="pug">
     header.adminheader
-        .admin-container            
+        .admin-container 
+            .my-img
             h1  Георгий Исаев
             h2 Панель администрирования
+            a.admin-exit(href="") Выйти           
 
 </template>
 
-<style lang='pcss' scoped>
-h1 {
-  margin: 0;
-  font-size: 18px;
-  margin-right: 28px;
-}
-h2 {
-    margin: 0;
-    font-size: 14px;
-    color: rgba(#fff, .5)
-}
-.adminheader {
-    width: 100%;
-    height: 80px;
-    background: linear-gradient(90deg, #3e3e59 0%, #454573 100%);
-}
-.admin-container {
-    display: flex;
-    margin: 0 auto;
-    max-width: 1480px;
-    width: 95%;
-    position: relative;  
-    align-items: center;
-    height: 100%;
-
-}
+<style lang='pcss' scoped src='../styles/adminheader.pcss'>
 
 </style>
